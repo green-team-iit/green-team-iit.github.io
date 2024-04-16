2948f60cde2d2dbe82ea7b5bdfc459a037df90a0
ThrishaliN <166986380+ThrishaliN@users.noreply.github.com>
2024-04-16 06:07:28 +0530

    Delete splash.js

--- splash.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded",function () {
-	setTimeout(function () {
-		
-		document.getElementById("splash").style.display = "none";
-		document.getElementById("content").style.display = "block";
-		addLiveAnimations();
-	}, 4000);
-	
-});
